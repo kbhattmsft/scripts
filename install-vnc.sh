8e3bd58fa99f8f86b7eaaf0ce44ec4f4b48f19c6
Kaushik Bhattacharya <kbhatt@microsoft.com>
2017-05-02 11:50:13 +0200

    adding GNOME

--- install-vnc.sh
+++ install-vnc.sh
@@ -3,13 +3,10 @@
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
 #install deps
+yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
 yum -y install xrdp tigervnc-server
+yum -y update
 #configure service
 systemctl start xrdp.service
 systemctl enable xrdp.service
-#change firewall
-firewall-cmd --permanent --zone=public --add-port=3389/tcp
-firewall-cmd --reload
-#apply SELinux
-chcon --type=bin_t /usr/sbin/xrdp
-chcon --type=bin_t /usr/sbin/xrdp-sesman
+shutdown -r now
