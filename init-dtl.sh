538356a60784545092f89271ebd3c6090d2db43a
Kaushik Bhattacharya <kbhatt@microsoft.com>
2017-05-08 13:44:03 +0200

    adding silent install

--- init-dtl.sh
+++ init-dtl.sh
@@ -7,7 +7,7 @@ yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
-yum install docker-ce
+yum install -y docker-ce
 systemctl start docker
 systemctl enable docker
 #user management
